b7cd8bda4878734ef5d346fae49ea33c8f149e97
zhaoxiaogang <286505491@qq.com>
2022-07-29 18:30:41 +0800

    impl aleo account generation.

--- src/cmd/gen_account.rs
+++ src/cmd/gen_account.rs
@@ -1,4 +1,6 @@
-
+use std::fs::File;
+use std::io::{BufRead, BufReader, BufWriter, prelude::*};
+use snarkvm::{dpc::testnet2::Testnet2, prelude::Account};
 use log::{info};
 use structopt::StructOpt;
 use anyhow::Result;
@@ -9,13 +11,39 @@ use anyhow::Result;
 pub struct Cmd {
     ///The config file which stored the generated account.
     #[structopt(long)]
-    file: String,
+    file: Option<String>,
 }
 
 impl Cmd {
 
     pub async fn run(&self) -> Result<()> {
         info!("Start to run generating account command!!!");
+        let account = Account::<Testnet2>::new(&mut rand::thread_rng());
+        let private_key = format!("Private key:  {}", account.private_key());
+        let view_key = format!("View key:  {}", account.view_key());
+        let address = format!("Address:  {}", account.address());
+
+        info!("{}", private_key);
+        info!("{}", view_key);
+        info!("{}", address);
+
+        info!("WARNING: Make sure you have a backup of both private key and view key!");
+        info!("Nobody can help you recover those keys if you lose them!");
+
+        if self.file.is_some() {
+            let content = format!("{}\n{}\n{}", private_key, view_key, address);
+            return self.backup_account(&content).await;
+        }
+
+        Ok(())
+    }
+
+    async fn backup_account(&self, content: &String)->Result<()> {
+        let file = File::create(self.file.as_ref().unwrap())?;
+        let mut writer = BufWriter::new(file);
+
+        writer.write_all((content.to_string()).as_bytes()).expect("write failed");
+        writer.flush().expect("flush failed");
 
         Ok(())
     }
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,11 @@ mod client;
 mod worker;
 mod stats;
 
+use std::process;
 use structopt::StructOpt;
 use log::{info};
 use log4rs;
+use anyhow::Result;
 use cmd::gen_account;
 
 #[derive(Debug, StructOpt)]
@@ -20,7 +22,23 @@ pub enum Cmd {
     GenAccount(gen_account::Cmd),
 }
 
-fn main() {
+async fn run(cli: Cli) -> Result<()> {
+    match cli.cmd {
+        Cmd::GenAccount(cmd) => cmd.run().await,
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()>{
     log4rs::init_file("config/config.yaml", Default::default()).unwrap();
     info!("Hello, world!");
+
+    let cli = Cli::from_args();
+
+    if let Err(e) = run(cli).await {
+        eprintln!("error: {:?}", e);
+        process::exit(1);
+    }
+
+    Ok(())
 }
