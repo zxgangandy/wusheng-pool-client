01b13dfe436c5053fe48dba51ddaee2481344986
zhaoxiaogang <286505491@qq.com>
2022-09-23 16:43:04 +0800

    mgr

--- src/mining/miner.rs
+++ src/mining/miner.rs
@@ -27,9 +27,6 @@ use crate::stats::Stats;
 
 pub struct Miner {
     pool: ThreadPool,
-    //total_proofs: Arc<AtomicU32>,
-    //running: AtomicBool,
-    //stats: Stats,
     miner_sender: Sender<MinerEvent>,
     miner_receiver: Receiver<MinerEvent>,
     stats: Arc<Stats>,
@@ -44,16 +41,7 @@ pub enum MinerEvent {
 
 impl Miner {
     pub fn new(index: u8, threads: u16, stats: Arc<Stats>,) -> Self {
-        //let core_threads = num_cpus::get() as u16 / max_core;
         let (miner_sender, miner_receiver) = channel::<MinerEvent>(256);
-        // for index in 0..max_core {
-        //     let pool = ThreadPoolBuilder::new()
-        //         .stack_size(8 * 1024 * 1024)
-        //         .num_threads(core_threads as usize)
-        //         .thread_name(move |idx| format!("cpu-core-{}-{}", index, idx))
-        //         .build().unwrap();
-        //     thread_pools.push(Arc::new(pool));
-        // }
 
         let pool = ThreadPoolBuilder::new()
             .stack_size(8 * 1024 * 1024)
@@ -63,9 +51,6 @@ impl Miner {
 
         Miner {
             pool,
-            //total_proofs: Arc::new(Default::default()),
-            //running: AtomicBool::new(false),
-            //stats: Stats::new(),
             miner_sender,
             miner_receiver,
             stats,
@@ -76,46 +61,13 @@ impl Miner {
         self.miner_sender.clone()
     }
 
-    fn running(&self) -> bool {
-        self.running.load(Ordering::SeqCst)
-    }
-
-    pub async fn stop(&self) {
-        if self.running() {
-            // let sender = self.prover_router.read().await;
-            // let (tx, rx) = oneshot::channel();
-            // if let Err(err) = sender.send(ProverMsg::Exit(tx)).await {
-            //     error!("failed to stop prover: {err}");
-            // }
-            // rx.await.unwrap();
-            debug!("miner exited");
-            self.running.store(false, Ordering::SeqCst);
-        }
-    }
-
-    pub fn start(&mut self,
-             // prover_router: Sender<ProverMsg>,
-             // statistic_router: Sender<StatisticMsg>,
-             // client_router: Sender<ClientMsg>,
-             // gpu_index: i16,
-             // threads: u8,
-    )  {
-        // let (tx, mut rx) = mpsc::channel(100);
-        // let worker = Worker {
-        //     pool: ThreadPoolBuilder::new().num_threads(threads as usize).build().unwrap(),
-        //     terminator: Arc::new(AtomicBool::new(false)),
-        //     ready: Arc::new(AtomicBool::new(true)),
-        //     prover_router,
-        //     statistic_router,
-        //     client_router,
-        // };
-
+    pub fn start(&mut self, )  {
         task::spawn(async move {
             while let Some(msg) = self.miner_receiver.recv().await {
                 match msg {
-                    MinerEvent::NewWork(template, diff) => worker.new_work(template, diff, gpu_index).await,
+                    MinerEvent::NewWork(difficulty, message) => self.new_work().await,
                     MinerEvent::Exit(responder) => {
-                        worker.wait_for_terminator();
+                        self.wait_for_terminator();
                         responder.send(()).expect("failed response exit msg");
                         break;
                     }
@@ -132,7 +84,7 @@ impl Miner {
         self.terminator.store(false, Ordering::SeqCst);
     }
 
-    async fn new_work(self: &Arc<Self>, template: Arc<BlockTemplate<Testnet2>>, share_difficulty: u64, gpu_index: i16) {
+    async fn new_work(&self) {
         let height = template.block_height();
         debug!("starting new work: {}", height);
         self.wait_for_terminator();
